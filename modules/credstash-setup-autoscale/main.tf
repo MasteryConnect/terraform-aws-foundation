3147710aa3fb7f11640c5bfbfa06de590ddbb3a3
Joshua Morris <joshuamorris3@users.noreply.github.com>
2021-01-31 23:15:33 +0000

    Updating index target

--- modules/credstash-setup-autoscale/main.tf
+++ modules/credstash-setup-autoscale/main.tf
@@ -118,9 +118,9 @@ resource "aws_appautoscaling_policy" "credstash-table-write-policy" {
 }
 
 output "credstash_writer_policy_arn" {
-  value = aws_iam_policy.writer-policy[count.index].arn
+  value = length(aws_iam_policy.writer-policy) > 0 ? aws_iam_policy.writer-policy[0].arn : null
 }
 
 output "credstash_reader_policy_arn" {
-  value = aws_iam_policy.reader-policy[count.index].arn
+  value = length(aws_iam_policy.reader-policy) > 0 ? aws_iam_policy.reader-policy[0].arn : null
 }
